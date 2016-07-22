8f2992f163458ad99c419d1278415912ed466b81
Milo Toor <mtoor@omicia.com>
2016-07-22 08:25:45 -0700

    Add fade to project tiles

--- src/client/components/ProjectGrid.jsx
+++ src/client/components/ProjectGrid.jsx
@@ -1,21 +1,20 @@
 
 import React from 'react';
 import {GridList, GridTile} from 'material-ui/GridList';
-import IconButton from 'material-ui/IconButton';
-import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+import transitions from 'material-ui/styles/transitions';
 
 const styles = {
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around'
-  },
-  gridList: {
-    width: '100%',
-    overflowY: 'auto',
-    marginBottom: 24
-  },
-    gridTile: {
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around'
+    },
+    gridList: {
+        width: '100%',
+        overflowY: 'auto',
+        marginBottom: 24
+    },
+    projectTile: {
         cursor: 'pointer'
     }
 };
@@ -32,24 +31,45 @@ class ProjectGrid extends React.Component {
                     padding={10}
                 >
                     {this.props.projects.map((project, index) => (
-                        <GridTile
-                            key={index}
-                            title={project.Project}
-                            subtitle={<span>by <b>{project.author}</b></span>}
-                            actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
-                            style={styles.gridTile}
-                            onClick={() => ProjectGrid.clicked(project)}
-                        >
-                            <img src={project['Image URL']} />
-                        </GridTile>
+                        <ProjectTile key={index} project={project} />
                     ))}
                 </GridList>
             </div>
         );
     }
+}
 
-    static clicked (project) {
-        window.location = `/#/${project.Project}`;
+
+class ProjectTile extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            hover: false
+        };
+    }
+
+    render () {
+        let imgStyle = {
+            transition: transitions.easeOut(null, 'opacity'),
+            opacity: this.state.hover ? 1 : 0.5
+        };
+
+        return (
+            <GridTile
+                title={this.props.project.Project}
+                subtitle={<span>by <b>{this.props.project.author}</b></span>}
+                style={styles.projectTile}
+                onClick={this.clicked}
+                onMouseOver={() => this.setState({ hover: true  })}
+                onMouseOut={ () => this.setState({ hover: false })}
+            >
+                <img style={imgStyle} src={this.props.project['Image URL']} />
+            </GridTile>
+        );
+    }
+
+    clicked = () => {
+        window.location = `/#/${this.props.project.Project}`;
     }
 }
 
