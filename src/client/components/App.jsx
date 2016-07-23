0aa9978d9e2b99c7d21d936080acde6e3ef8969d
Milo Toor <mtoor@omicia.com>
2016-07-22 20:05:43 -0700

    Add header, wizard font

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -8,12 +8,20 @@ const styles = {
     appContainer: {
         width: 1000,
         margin: 'auto'
+    },
+    appHeader: {
+        fontSize: 60,
+        textAlign: 'center',
+        margin: '10px 0',
+        fontFamily: 'Wizard, Roboto'
     }
 };
+
 
 let shuffledProjects = _.once(() => _.shuffle(window.projects));
 let App = () => (
     <div style={styles.appContainer}>
+        <h1 style={styles.appHeader}>Servant Projects</h1>
         <ProjectGrid projects={shuffledProjects()} />
     </div>
 );
